5000b7cead2a695339854df21fb68dc7f2161b2c
robin-lu <1183906858@qq.com>
2026-04-26 01:10:53 +0800

    chore: deep clean temp_scripts to remove massive historical one-off files

--- temp_scripts/repair_space_sync.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("data/space_mapping.json", "r", encoding="utf-8") as f:
-    mapping = json.load(f)
-print("Mapping fields:")
-print(json.dumps(mapping, indent=2, ensure_ascii=False))
