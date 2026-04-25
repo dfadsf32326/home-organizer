feceb413ddccfa035ef75969b117b5c3190a54ad
robin-lu <1183906858@qq.com>
2026-04-26 01:10:31 +0800

    chore: strictly remove duplicate scripts returned by rsync

--- fix_token.py
+++
@@ -1,8 +0,0 @@
-import os
-path = "/Users/robinlu/.hermes/skills/custom/home-organizer/scripts/sync_spaces_bidirectional.py"
-content = open(path).read()
-lines = content.split('\n')
-for i, line in enumerate(lines):
-    if "BASE_TOKEN = " in line:
-        lines[i] = 'BASE_TOKEN="PS56bPNKtaH7GjsM9LfcBfJOnJb"'
-open(path, 'w').write('\n'.join(lines))
